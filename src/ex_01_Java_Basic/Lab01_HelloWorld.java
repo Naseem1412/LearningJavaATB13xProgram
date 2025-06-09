e59393b4e8e6390055eea2e903142c6494b9ca22
naseemashaik <naseemashaik335@gmail.com>
2025-06-09 23:36:20 +0530

    New added Lab0001 added

---
+++ src/ex_01_Java_Basic/Lab01_HelloWorld.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab01_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello WORLD");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab02_HW_Extra.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab02_HW_Extra {
+    public static void main(String[] args) {
+        System.out.println(14+12);
+    }
+}
