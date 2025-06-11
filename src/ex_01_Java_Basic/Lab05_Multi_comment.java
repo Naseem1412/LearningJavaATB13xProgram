c5302799c03e109122149cdd3f9502964e569c0f
naseemashaik <naseemashaik335@gmail.com>
2025-06-11 20:31:04 +0530

    11th June

---
+++ src/ex_01_Java_Basic/Lab05_Multi_comment.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+import java.sql.SQLOutput;
+
+public class Lab05_Multi_comment {
+    public static void main(String[] args) {
+     /*
+     * Author : Naseema Shaik
+     * Course : Java
+     *
+     */
+        System.out.println("Multi comment");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab06_Keyword_Identifier.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class Lab06_Keyword_Identifier {
+    public static void main(String[] args) {
+        System.out.println("How many keywords do you see there?,identifier");
+    }
+}
